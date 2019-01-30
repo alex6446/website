7739b4a6729886540b3a441c7ce2dbda8367556d
DJEscHGL <xxxdjescxxx@gmail.com>
2019-01-30 18:31:01 +0300

    Starting...

---
+++ www/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <title>Project Paralax</title>
+</head>
+<body>
+  <div class="container-fluid">
+    <h1>Welcom to Project Paralax</h1>
+  </div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="../js/popper.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+</body>
+</html>
