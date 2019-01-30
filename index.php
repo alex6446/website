5262067ce5d8ef61ae99e96e7258ff897f2ad9b0
DJEscHGL <xxxdjescxxx@gmail.com>
2019-01-30 21:06:47 +0300

    remake

--- index.php
+++ index.php
@@ -4,11 +4,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <title>Project Paralax</title>
+    <title>Paralax Project</title>
 </head>
 <body>
   <div class="container-fluid">
-    <h1>Welcom to Project Paralax</h1>
+    <h1>Welcom to Paralax Project</h1>
+    Future plans:
+    *desing
+    *registration
+    *chat
+    *github api
   </div>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="../js/popper.min.js"></script>
